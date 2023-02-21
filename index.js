35e74d77d78c4894762803335c4743e47f67aef7
Lukas Hass <lukas@slucky.de>
2023-02-21 22:27:02 +0100

    chore: update dependencies

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 module.exports = {
   plugins: [
     [
-      "@semantic-release/commit-analyzer",
+      '@semantic-release/commit-analyzer',
       {
-        preset: "angular",
+        preset: 'angular',
         releaseRules: [
-          // build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test
-          { type: "build", release: "patch" },
-          { type: "chore", release: "patch" },
-          { type: "ci", release: "patch" },
-          { type: "refactor", release: "patch" },
-          { type: "revert", release: "patch" },
+          // Available types: build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test
+          {type: 'build', release: 'patch'},
+          {type: 'chore', release: 'patch'},
+          {type: 'ci', release: 'patch'},
+          {type: 'refactor', release: 'patch'},
+          {type: 'revert', release: 'patch'},
         ],
       },
     ],
-    "@semantic-release/commit-analyzer",
+    '@semantic-release/commit-analyzer',
     // '@semantic-release/changelog',
-    "@semantic-release/release-notes-generator",
-    ["@semantic-release/npm", { npmPublish: true }],
-    "@semantic-release/github",
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/npm', {npmPublish: true}],
+    '@semantic-release/github',
   ],
-  branches: ["main"],
+  branches: ['main'],
 };
